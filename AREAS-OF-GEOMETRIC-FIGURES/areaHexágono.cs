d0e4078ffad98799eaf0d02a01f42590e15cb37a
Ismael Martínez <121949680+ISMARZDEV@users.noreply.github.com>
2023-01-26 22:28:48 -0400

    Update areaHexágono.cs

--- AREAS-OF-GEOMETRIC-FIGURES/areaHexágono.cs
+++ AREAS-OF-GEOMETRIC-FIGURES/areaHexágono.cs
@@ -1,27 +1,27 @@
 using System;
 
-namespace ConsoleApp
+namespace ConsoleApp5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            double BaseMayor, BaseMenor, Altura;
-            double areaTrapecio;
+            double Apotema, Perimetro;
+            double areaHexagono;
 
-            Console.Write("Digite la Base Mayor: ");
-            BaseMayor = Convert.ToDouble(Console.ReadLine());
+            // Ask the user to enter the apotema of the hexagon
+            Console.Write("Digite el Apotema: ");
+            Apotema = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Digite la Base Menor: ");
-            BaseMenor = Convert.ToDouble(Console.ReadLine());
+            // Ask the user to enter the perimeter of the hexagon
+            Console.Write("Digite el Perimetro: ");
+            Perimetro = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Digite la Altura: ");
-            Altura = Convert.ToDouble(Console.ReadLine());
+            // Calculate the area of the hexagon
+            areaHexagono = (Apotema * Perimetro) / 2;
 
-            areaTrapecio = (BaseMayor + BaseMenor) * Altura / 2;
-
-            Console.WriteLine("El área del Trapecio es ({0} + {1}) x {2} / 2 = {3:N2}cm²", BaseMayor, BaseMenor, Altura, areaTrapecio);
-            
+            // Print the result of the calculation on the screen
+            Console.WriteLine("El área del Hexagono es ({0} x {1}) / 2 = {2:N2}cm²", Apotema, Perimetro, areaHexagono);
         }
     }
 }
