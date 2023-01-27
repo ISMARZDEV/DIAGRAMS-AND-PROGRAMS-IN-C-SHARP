03afb8d051df46ee48db6fe907eaba739cad30a3
Ismael Martínez <121949680+ISMARZDEV@users.noreply.github.com>
2023-01-26 21:35:14 -0400

    Create SCUAREAREA.cs

---
+++ SQUARE AREA/SCUAREAREA.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lado1, lado2;
+            int areaCuadrado;
+
+            Console.Write("Digite el Lado #1: ");
+            lado1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite el Lado #2: ");
+            lado2 = Convert.ToInt32(Console.ReadLine());
+
+            areaCuadrado = (lado1 * lado2);
+
+            Console.WriteLine("El área del cuadrado es {0} x {1} = {2}cm²", lado1, lado2, areaCuadrado);
+            
+        }
+    }
+}
