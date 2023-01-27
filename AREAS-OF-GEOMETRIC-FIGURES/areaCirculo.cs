c60b3c1b9b4b2f2c05ab5b54604486795585058b
Ismael Martínez <121949680+ISMARZDEV@users.noreply.github.com>
2023-01-26 22:20:15 -0400

    Update areaCirculo.cs

--- AREAS-OF-GEOMETRIC-FIGURES/areaCirculo.cs
+++ AREAS-OF-GEOMETRIC-FIGURES/areaCirculo.cs
@@ -4,18 +4,23 @@ namespace ConsoleApp
 {
     internal class Program
     {
-        
-
+        // Entry point of the program
         static void Main(string[] args)
         {
+            // Declare variable to store the radius
             double Radio;
+            // Declare a variable to store the area of the circle
             double areaCirculo;
 
+            // Prompt the user to enter the value for the radius
             Console.Write("Digite el Radio: ");
+            // Read and store the user input in the variable Radio
             Radio = Convert.ToDouble(Console.ReadLine());
             
+            // Calculate the area of the circle using the formula pi * r^2
             areaCirculo = (Math.PI) * (Math.Pow(Radio,2));
 
+            // Display the result of the calculation, including the input values and the final area
             Console.WriteLine("El área del Circulo es ({0:N2} x {1}²) = {2:N2}cm²", Math.PI, Radio, areaCirculo);
         }
     }
